8a8721796d2e23ea89ad539f50537d7a571fbe1b
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-07-02 23:54:07 +0000

    restarting

--- src/copper.py
+++
@@ -1,6 +0,0 @@
-import utils.parser as parser
-
-print("Enter path to file:")
-filepath = input("> ")
-
-print(parser.parse(filepath))
--- src/utils/parser.py
+++
@@ -1,7 +0,0 @@
-def parse(file: str) -> list:
-    lines = []
-    with open(file, 'r') as f:
-        for line in f.readlines():
-            lines.append(line)
-    
-    return lines
