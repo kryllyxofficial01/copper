cc06cbd24bb3abc85f459f1f11671eb91fcf188c
Kryllyx <kryllyxofficial@gmail.com>
2022-07-12 15:37:40 -0400

    lexing for output, input, and variables

--- src/copper/lexer.py
+++ src/copper/lexer.py
@@ -1,6 +1,9 @@
 import keyword
 from copper.error import Error
 
+DIGITS = "0123456789"
+LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 class Lexer:
     def __init__(self, line, lineno, file):
         self.line = line
@@ -13,6 +16,7 @@ class Lexer:
         if self.line[:4] == "exec":
             self.tokens["KEYWORD"] = "exec"
             command = list(self.line)
+            command.pop()
             
             for char in "exec":
                 command.remove(char)
@@ -20,6 +24,9 @@ class Lexer:
             
             if command[:3] == ["o", "u", "t"]:
                 self.lex_output(command)
+            
+            elif command[:2] == ["i", "n"]:
+                self.lex_input(command)
             
             else:
                 i = 0
@@ -33,6 +40,16 @@ class Lexer:
                 
                 error = Error("CommandError", f"Unknown command \"{command_name}\"", self.line, self.lineno, self.file)
                 error.print_stacktrace()
+        
+        elif self.line[:3] == "set":
+            self.tokens["KEYWORD"] = "set"
+            command = list(self.line)
+        
+            for char in "set":
+                command.remove(char)
+            command.remove(" ")
+            
+            self.lex_vars(command)
         
         else:
             i = 0
@@ -68,5 +85,63 @@ class Lexer:
                 args += command[i]
                 i += 1
             
-            self.tokens["STRING"] = args
+            self.tokens["STRING"] = "".join(args)
             self.tokens["RPAREN"] = ")"
+        
+        else:
+            error = Error("SyntaxError", "Missing parentheses", self.line, self.lineno, self.file)
+            error.print_stacktrace()
+    
+    def lex_input(self, command):
+        self.tokens["COMMAND"] = "in"
+        
+        for char in "in":
+            command.remove(char)
+        
+        if command[0] == "(" and command[-1] == ")":
+            self.tokens["LPAREN"] = "("
+            command.remove("(")
+            
+            i = 0
+            args = ""
+            while command[i] != ")":
+                args += command[i]
+                i += 1
+            
+            self.tokens["STRING"] = "".join(args)
+            self.tokens["RPAREN"] = ")"
+            
+        else:
+            error = Error("SyntaxError", "Missing parentheses", self.line, self.lineno, self.file)
+            error.print_stacktrace()
+    
+    def lex_vars(self, command):
+        i = 0
+        var_name = ""
+        while True:
+            if command[i] == "=" or command[i] == " ":
+                break
+            else:
+                var_name += command[i]
+                i += 1
+        
+        self.tokens["VARNAME"] = var_name
+        
+        for char in var_name:
+            command.remove(char)
+        if command[0] == " ":
+            command.remove(" ")
+        
+        if command[0] == "=":
+            self.tokens["EQUALS"] = "="
+        else:
+            error = Error("SyntaxError", "Missing equals sign", self.line, self.lineno, self.file)
+            error.print_stacktrace()
+        
+        if command[1] == " ":
+            command.remove("=")
+            command.remove(" ")
+        else:
+            command.remove("=")
+        
+        self.tokens["CONTENT"] = "".join(command)
--- src/main.py
+++ src/main.py
@@ -8,12 +8,21 @@ contents = file.readlines()
 
 for line in contents:
     line = list(line)
-    line.pop()
-    line = str(line)
+    
+    if line[-1] == "\n":
+        line.pop()
+        
+    line = "".join(line)
 
 lineno = 1
 for line in contents:
-    lexer = Lexer(line, lineno, filepath)
-    tokens = lexer.lex()
+    if line == "\n" or line[:2] == "//":
+        pass
     
+    else:
+        lexer = Lexer(line, lineno, filepath)
+        tokens = lexer.lex()
+    
+    print(tokens)
+        
     lineno += 1
